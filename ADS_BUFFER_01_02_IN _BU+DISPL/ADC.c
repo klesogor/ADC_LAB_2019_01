754a3ad1ca3d9a529469e63ac75b8841aac923c8
Kirill Lesohorskyi <lesogor.kirill@gmail.com>
2019-11-14 09:25:12 +0200

    Implemented ADC (#2)

--- ADS_BUFFER_01_02_IN _BU+DISPL/ADC.c
+++ ADS_BUFFER_01_02_IN _BU+DISPL/ADC.c
@@ -49,13 +49,13 @@ void ADC_init(void)
     GPIO_InitTypeDef  GPIO_InitStructure;
     // input of ADC (it doesn't seem to be needed, as default GPIO state is floating input)
     GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_AIN;
-    GPIO_InitStructure.GPIO_Pin   = GPIO_Pin_1 ;        // that's ADC1 (PA1 on STM32)
+    GPIO_InitStructure.GPIO_Pin   = GPIO_Pin_2 ;        // that's ADC2 (PA1 on STM32)
     GPIO_Init(GPIOA, &GPIO_InitStructure);
  
     //clock for ADC (max 14MHz --> 72/6=12MHz)
     RCC_ADCCLKConfig (RCC_PCLK2_Div6);
     // enable ADC system clock
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC2, ENABLE);
  
     // define ADC config
     ADC_InitStructure.ADC_Mode = ADC_Mode_Independent;
@@ -65,26 +65,26 @@ void ADC_init(void)
     ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
     ADC_InitStructure.ADC_NbrOfChannel = 1;
  
-    ADC_RegularChannelConfig(ADC1,ADC_Channel_1, 1,ADC_SampleTime_28Cycles5); // define regular conversion config
-    ADC_Init ( ADC1, &ADC_InitStructure);   //set config of ADC1
+    ADC_RegularChannelConfig(ADC2,ADC_Channel_1, 1,ADC_SampleTime_28Cycles5); // define regular conversion config
+    ADC_Init ( ADC2, &ADC_InitStructure);   //set config of ADC2
  
     // enable ADC
-    ADC_Cmd (ADC1,ENABLE);  //enable ADC1
+    ADC_Cmd (ADC2,ENABLE);  //enable ADC2
  
     //  ADC calibration (optional, but recommended at power on)
-    ADC_ResetCalibration(ADC1); // Reset previous calibration
-    while(ADC_GetResetCalibrationStatus(ADC1));
-    ADC_StartCalibration(ADC1); // Start new calibration (ADC must be off at that time)
-    while(ADC_GetCalibrationStatus(ADC1));
+    ADC_ResetCalibration(ADC2); // Reset previous calibration
+    while(ADC_GetResetCalibrationStatus(ADC2));
+    ADC_StartCalibration(ADC2); // Start new calibration (ADC must be off at that time)
+    while(ADC_GetCalibrationStatus(ADC2));
  
     // start conversion
-    ADC_Cmd (ADC1,ENABLE);  //enable ADC1
-    ADC_SoftwareStartConvCmd(ADC1, ENABLE); 
+    ADC_Cmd (ADC2,ENABLE);  //enable ADC1
+    ADC_SoftwareStartConvCmd(ADC2, ENABLE); 
 }
 
 unsigned int ADC_read()
 {
 	delay_ms(10);
-	return ADC_GetConversionValue(ADC1);
+	return ADC_GetConversionValue(ADC2);
 	delay_ms(10);
 }
