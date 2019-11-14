6e9c857edd350272155f0fc4b97d0d730a022be2
Kirill Lesohorskyi <lesogor.kirill@gmail.com>
2019-11-14 11:27:32 +0200

    Implemented timers

--- ADS_BUFFER_01_02_IN _BU+DISPL/RTE/_Target_1/RTE_Components.h
+++ ADS_BUFFER_01_02_IN _BU+DISPL/RTE/_Target_1/RTE_Components.h
@@ -1,6 +1,6 @@
 
 /*
- * Auto generated Run-Time-Environment Configuration File
+ * Auto generated Run-Time-Environment Component Configuration File
  *      *** Do not modify ! ***
  *
  * Project: 'UART to RF24' 
@@ -16,16 +16,11 @@
  */
 #define CMSIS_device_header "stm32f10x.h"
 
-/*  Keil::Device:StdPeriph Drivers:ADC:3.5.0 */
 #define RTE_DEVICE_STDPERIPH_ADC
-/*  Keil::Device:StdPeriph Drivers:Framework:3.5.1 */
 #define RTE_DEVICE_STDPERIPH_FRAMEWORK
-/*  Keil::Device:StdPeriph Drivers:GPIO:3.5.0 */
 #define RTE_DEVICE_STDPERIPH_GPIO
-/*  Keil::Device:StdPeriph Drivers:RCC:3.5.0 */
 #define RTE_DEVICE_STDPERIPH_RCC
-/*  Keil::Device:StdPeriph Drivers:USART:3.5.0 */
+#define RTE_DEVICE_STDPERIPH_TIM
 #define RTE_DEVICE_STDPERIPH_USART
-
 
 #endif /* RTE_COMPONENTS_H */
